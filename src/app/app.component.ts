c4b5b84291fa9284a1c9e9ed49ecd1fca00224b7
Oskar <omikolaj1@gmail.com>
2019-09-08 16:38:41 -0400

    Add and configure angular-auth-oidc- client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'IsP-Angular';
+  isAuthenticated: boolean;
+  userData: any;
+
+  constructor(private http: HttpClient, public oidcSecurityService: OidcSecurityService) {
+    if (this.oidcSecurityService.moduleSetup) {
+      this.doCallbackLogicIfRequired();
+    } else {
+      this.oidcSecurityService.onModuleSetup.subscribe(() => {
+        this.doCallbackLogicIfRequired();
+      });
+    }
+  }
+
+  ngOnInit() {
+    this.oidcSecurityService.getIsAuthorized().subscribe(auth => {
+      this.isAuthenticated = auth;
+    });
+
+    this.oidcSecurityService.getUserData().subscribe(userData => {
+      this.userData = userData;
+    });
+  }
+
+  onLogin() {
+    console.log("onLogin executed");
+    this.oidcSecurityService.authorize();
+  }
+
+  onLogout() {
+    console.log("onLogout exeucted");
+    this.oidcSecurityService.logoff();
+  }
+  private doCallbackLogicIfRequired() {
+    // Will do a callback, if the url has a code and state parameter.
+    this.oidcSecurityService.authorizedCallbackWithCode(window.location.toString());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthModule, ConfigResult, OidcConfigService, OidcSecurityService, OpenIdConfiguration } from 'angular-auth-oidc-client';
+
+export function loadConfig(oidcConfgService: OidcConfigService) {
+  return () => oidcConfgService.load_using_stsServer('http://localhost:5000');
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +18,42 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    AuthModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfig,
+      deps: [OidcConfigService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private oidcSecurityService: OidcSecurityService, private oidcConfigService: OidcConfigService) {
+    this.oidcConfigService.onConfigurationLoaded.subscribe((configResult: ConfigResult) => {
+
+      // Use the configResult to set the configurations
+
+      const config: OpenIdConfiguration = {
+        stsServer: configResult.customConfig.stsServer,
+        redirect_url: 'http://localhost:4201',
+        client_id: 'spa',
+        scope: 'openid profile email',
+        response_type: 'code',
+        silent_renew: true,
+        silent_renew_url: 'http://localhost:4201/silent-renew.html',
+        log_console_debug_active: true,
+        // all other properties you want to set
+      };
+
+      this.oidcSecurityService.setupModule(config, configResult.authWellknownEndpoints);
+    });
+  }
+}
